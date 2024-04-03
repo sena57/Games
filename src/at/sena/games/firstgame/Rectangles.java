799cacbf85d195d1ffcf0a8efa67d425f6859d03
sena57 <126655518+sena57@users.noreply.github.com>
2024-04-03 09:32:21 +0200

    programmed rectangle game

--- src/at/sena/games/firstgame/Rectangles.java
+++ src/at/sena/games/firstgame/Rectangles.java
@@ -1,27 +1,45 @@
 package at.sena.games.firstgame;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
+    private int x;
+    private int y;
+
     public Rectangles(String title) {
         super(title);
     }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-
+        this.x = 100;
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
+        this.x++;
+        this.y++;
 
+        if(this.x > 800){
+            this.x=0;
+            this.y=0;
+        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawRect(this.x,this.y,100,100);
+        graphics.drawString("Hellooo",50,50);
+    }
 
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
