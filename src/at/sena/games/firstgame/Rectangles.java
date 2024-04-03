d438697c973b279f045cd3c9bb8e17cf50a662d3
sena57 <126655518+sena57@users.noreply.github.com>
2024-04-03 08:38:12 +0200

    first commit

---
+++ src/at/sena/games/firstgame/Rectangles.java
@@ -0,0 +1,27 @@
+package at.sena.games.firstgame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Rectangles extends BasicGame {
+    public Rectangles(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
